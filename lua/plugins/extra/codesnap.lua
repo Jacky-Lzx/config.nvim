3a48b383931b436de57665b5c4c56f88128af350
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-10-30 02:55:06 +0800

    feat(codesnap): added a plugin to take snaps of the code.

---
+++ lua/plugins/extra/codesnap.lua
@@ -0,0 +1,23 @@
+return {
+  -- A plugin to take beautiful screenshots of your code
+  {
+    "mistricky/codesnap.nvim",
+    build = "make",
+    opts = {
+      mac_window_bar = false,
+      -- title = "CodeSnap.nvim",
+      -- code_font_family = "CaskaydiaCove Nerd Font",
+      -- watermark_font_family = "Pacifico",
+      -- watermark = "CodeSnap.nvim",
+      -- bg_theme = "default",
+      breadcrumbs_separator = "/",
+      has_breadcrumbs = false,
+      has_line_number = true,
+      show_workspace = false,
+      min_width = 0,
+      -- Hide background
+      bg_padding = 0,
+      save_path = os.getenv("XDG_DOWNLOAD_DIR") or (os.getenv("HOME") .. "/Downloads"),
+    },
+  },
+}
