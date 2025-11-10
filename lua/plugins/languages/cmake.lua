003594407adf7bf76ca387d8aee40f7de98fa237
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-11-10 19:59:11 +0800

    feat(cmake): added LSP and formatter for cmake files.

---
+++ lua/plugins/languages/cmake.lua
@@ -0,0 +1,33 @@
+-- At this point (2025.11.10), the LSP has problem to start
+vim.lsp.enable("cmake") -- From Mason package "cmake-language-server"
+
+local M = {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = {
+      ensure_installed = { "cmake" },
+    },
+    opts_extend = { "ensure_installed" },
+  },
+
+  {
+    "mason-org/mason.nvim",
+    optional = true,
+    opts_extend = { "ensure_installed" },
+    opts = { ensure_installed = { "cmakelang", "cmake-language-server" } },
+  },
+
+  -- formatter
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        cmake = { "cmake_format" }, -- From Mason package "cmakelang"
+      },
+    },
+  },
+}
+
+return M
