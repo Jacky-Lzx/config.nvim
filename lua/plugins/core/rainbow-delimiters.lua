7b289120789a339ef2d73d610937557343b656d2
Zexi. Li <lzx12138@sjtu.edu.cn>
2025-11-21 18:39:16 +0800

    fix(rainbow-delimiters): disabled rainbow-delimiters for markdown files, as it causes an error when opening a markdown file with LaTeX code.

--- lua/plugins/core/rainbow-delimiters.lua
+++ lua/plugins/core/rainbow-delimiters.lua
@@ -3,7 +3,11 @@ return {
     "HiPhish/rainbow-delimiters.nvim",
     event = "BufReadPost",
     submodules = false,
-    config = true,
+    opts = {
+      -- Disable the parsing of markdown files.
+      -- Problem happens when there are LaTeX codes in a markdown file.
+      blacklist = { "markdown", "markdown_inline" },
+    },
     main = "rainbow-delimiters.setup",
   },
 }
