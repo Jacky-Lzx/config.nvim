822cad663edf9fbfe920099adfe9c86246f68a84
Zexi. Li <lzx12138@sjtu.edu.cn>
2026-01-04 12:26:06 +0800

    feat(avante): disabled avante.nvim plugin.

--- lua/plugins/extra/avante.lua
+++ lua/plugins/extra/avante.lua
@@ -1,10 +1,11 @@
 return {
   {
     "yetone/avante.nvim",
+    enabled = false,
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     -- ⚠️ must add this setting! ! !
     build = vim.fn.has("win32") == 1 and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
-      or "make",
+      or "make BUILD_FROM_SOURCE=true",
     event = "VeryLazy",
     version = false, -- Never set this value to "*"! Never!
     dependencies = {
