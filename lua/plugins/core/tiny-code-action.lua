3c7009765c9fe864d5e35f8dde3392822bdb13b0
Zexi. Li <lzx12138@sjtu.edu.cn>
2026-01-09 14:37:43 +0800

    feat(code-action): changed the layout style of code-action window.

--- lua/plugins/core/tiny-code-action.lua
+++ lua/plugins/core/tiny-code-action.lua
@@ -2,20 +2,14 @@ return {
   {
     "rachartier/tiny-code-action.nvim",
     dependencies = {
-      { "nvim-lua/plenary.nvim" },
-      {
-        "folke/snacks.nvim",
-        opts = {
-          -- terminal = {},
-        },
-      },
+      "nvim-lua/plenary.nvim",
+      "folke/snacks.nvim",
     },
     event = "LspAttach",
     -- stylua: ignore
     keys = {
-      { "<leader>a",   function() require("tiny-code-action").code_action({}) end, desc = "[LSP] Code action", noremap = true, silent = true, },
-      { "<A-a>",   function() require("tiny-code-action").code_action({}) end, desc = "[LSP] Code action", mode = "i", noremap = true, silent = true, },
-      -- { "<leader>gra", function() require("tiny-code-action").code_action({}) end, desc = "[LSP] Code action", noremap = true, silent = true, },
+      { "<leader>a", function() require("tiny-code-action").code_action({}) end, desc = "[LSP] Code action",             noremap = true, silent = true, },
+      { "<A-a>",     function() require("tiny-code-action").code_action({}) end, desc = "[LSP] Code action", mode = "i", noremap = true, silent = true, },
     },
     opts = {
       backend = "delta",
@@ -28,29 +22,7 @@ return {
           },
         },
       },
-      picker = {
-        "snacks",
-        opts = {
-          layout = {
-            reverse = false,
-            layout = {
-              backdrop = false,
-              width = 0.8,
-              min_width = 100,
-              height = 0.8,
-              min_height = 3,
-              box = "vertical",
-              border = "rounded",
-              title = "{title}",
-              title_pos = "center",
-              { win = "input", height = 1, border = "bottom" },
-              { win = "list", height = 5, border = "none" },
-              -- { win = "preview", title = "{preview}", height = 0.4, border = "top" },
-              { win = "preview", title = "{preview}", border = "top" },
-            },
-          },
-        },
-      },
+      picker = { "snacks", opts = {} },
     },
   },
 }
