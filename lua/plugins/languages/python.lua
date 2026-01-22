7bf36376334d57a4772ef06c8d1e90f58f1b6a2e
Zexi. Li <lzx12138@sjtu.edu.cn>
2026-01-22 13:26:38 +0800

    feat(python): added ruff_fix in conform.

--- lua/plugins/languages/python.lua
+++ lua/plugins/languages/python.lua
@@ -60,7 +60,7 @@ local M = {
     optional = true,
     opts = {
       formatters_by_ft = {
-        python = { "ruff_organize_imports", "ruff_format" },
+        python = { "ruff_fix", "ruff_organize_imports", "ruff_format" },
       },
     },
   },
