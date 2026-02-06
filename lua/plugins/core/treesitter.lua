5fc3566c79683c9023d6d8bc30bc507a150c15cb
Zexi. Li <lzx12138@sjtu.edu.cn>
2026-02-06 16:38:53 +0800

    fix(treesitter): fixed the treesitter to automatically enable on supported filetypes.

--- lua/plugins/core/treesitter.lua
+++ lua/plugins/core/treesitter.lua
@@ -1,3 +1,18 @@
+-- Automatically start treesitter for supported filetypes
+-- Reference materials:
+-- - https://github.com/nvim-treesitter/nvim-treesitter/issues/8308
+-- - https://github.com/lewis6991/ts-install.nvim/issues/9
+-- - https://github.com/nvim-treesitter/nvim-treesitter/discussions/7927
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function(args)
+    local lang = vim.treesitter.language.get_lang(args.match) or args.match
+    local installed = require("nvim-treesitter").get_installed("parsers")
+    if vim.tbl_contains(installed, lang) then
+      vim.treesitter.start(args.buf)
+    end
+  end,
+})
+
 return {
   -- 2026.02.06: This plugin is updated from the master branch to main branch.
   -- Some options are invalid anymore (all opts presented here)
