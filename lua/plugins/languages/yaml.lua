215e347d18b5860b9291168cda0bf40d58002f6e
Zexi. Li <lzx12138@sjtu.edu.cn>
2026-03-17 15:33:06 +0800

    feat(yaml): added LSP for yaml.

--- lua/plugins/languages/yaml.lua
+++ lua/plugins/languages/yaml.lua
@@ -1,3 +1,21 @@
+vim.lsp.config("yamlls", {
+  settings = {
+    yaml = {
+      format = {
+        -- This seems not working. The formatter is still enabled.
+        enable = false,
+      },
+      -- Associated a schema url, relative , or absolute (to root of project, not to filesystem root) path to the a glob
+      -- pattern relative to the detected project root.
+      schemas = {
+        ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
+      },
+    },
+  },
+})
+
+vim.lsp.enable("yamlls")
+
 local M = {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -17,6 +35,13 @@ local M = {
         yaml = { "prettierd" },
       },
     },
+  },
+
+  {
+    "mason-org/mason.nvim",
+    optional = true,
+    opts_extend = { "ensure_installed" },
+    opts = { ensure_installed = { "yaml-language-server" } },
   },
 }
 
