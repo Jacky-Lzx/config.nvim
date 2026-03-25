0b03eb08fbd9f57b59b28e03cebdc32675e03243
Zexi. Li <lzx12138@sjtu.edu.cn>
2026-03-25 16:27:03 +0800

    feat(matlab): added matlab support.

---
+++ lua/plugins/languages/matlab.lua
@@ -0,0 +1,32 @@
+vim.lsp.enable("matlab_ls")
+
+local M = {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    optional = true,
+    opts = {
+      ensure_installed = { "matlab" },
+    },
+    opts_extend = { "ensure_installed" },
+  },
+
+  -- formatter
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        matlab = { "mh_style" },
+      },
+    },
+  },
+
+  {
+    "mason-org/mason.nvim",
+    optional = true,
+    opts_extend = { "ensure_installed" },
+    opts = { ensure_installed = { "matlab-language-server", "miss_hit" } },
+  },
+}
+
+return M
