2b8c631c6f550c87de1459b42bea4a40aaff5938
Zexi. Li <lzx12138@sjtu.edu.cn>
2026-03-25 16:34:34 +0800

    feta(minuet): disabled minuet ai.

--- lua/plugins/extra/minuet.lua
+++ lua/plugins/extra/minuet.lua
@@ -1,8 +1,49 @@
 vim.api.nvim_set_hl(0, "BlinkCmpItemKindMinuet", { fg = "#a6e3a1" })
+
 return {
   {
     "milanglacier/minuet-ai.nvim",
+    cond = false,
+    dependencies = {
+      {
+        "saghen/blink.cmp",
+        optional = true,
+        opts = {
+          -- keymap = {
+          --   -- Manually invoke minuet completion.
+          --   ["<A-y>"] = require("minuet").make_blink_map(),
+          -- },
+          sources = {
+            default = { "minuet" },
+            providers = {
+              minuet = {
+                name = "minuet",
+                module = "minuet.blink",
+                async = true,
+                -- Should match minuet.config.request_timeout * 1000,
+                -- since minuet.config.request_timeout is in seconds
+                timeout_ms = 10000,
+                score_offset = 200, -- Gives minuet higher priority among suggestions
+                transform_items = function(_, items)
+                  for _, item in ipairs(items) do
+                    item.kind_icon = ""
+                    item.kind_name = "Local LM"
+                  end
+                  return items
+                end,
+              },
+            },
+          },
+          -- Recommended to avoid unnecessary request
+          completion = { trigger = { prefetch_on_insert = false } },
+        },
+      },
+    },
     opts = {
+      -- If completion item has multiple lines, create another completion item
+      -- only containing its first line. This option only has impact for cmp and
+      -- blink. For virtualtext, no single line entry will be added.
+      add_single_line_entry = false,
       provider = "openai_compatible", -- Use an OpenAI-compatible provider
       provider_options = {
         openai_compatible = {
@@ -20,38 +61,4 @@ return {
     },
   },
   { "nvim-lua/plenary.nvim" },
-
-  {
-    "saghen/blink.cmp",
-    optional = true,
-    opts = {
-      -- keymap = {
-      --   -- Manually invoke minuet completion.
-      --   ["<A-y>"] = require("minuet").make_blink_map(),
-      -- },
-      sources = {
-        default = { "minuet" },
-        providers = {
-          minuet = {
-            name = "minuet",
-            module = "minuet.blink",
-            async = true,
-            -- Should match minuet.config.request_timeout * 1000,
-            -- since minuet.config.request_timeout is in seconds
-            timeout_ms = 10000,
-            score_offset = 200, -- Gives minuet higher priority among suggestions
-            transform_items = function(_, items)
-              for _, item in ipairs(items) do
-                item.kind_icon = ""
-                item.kind_name = "Local LM"
-              end
-              return items
-            end,
-          },
-        },
-      },
-      -- Recommended to avoid unnecessary request
-      completion = { trigger = { prefetch_on_insert = false } },
-    },
-  },
 }
