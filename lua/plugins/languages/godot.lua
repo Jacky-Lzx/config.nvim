61e9f93e438704d946de7393bcbfc45a69afa928
Zexi. Li <lzx12138@sjtu.edu.cn>
2026-04-08 22:16:28 +0800

    feat(godot): added configs for godot development.

---
+++ lua/plugins/languages/godot.lua
@@ -0,0 +1,40 @@
+vim.lsp.enable("gdscript")
+
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts_extend = { "ensure_installed" },
+    opts = {
+      ensure_installed = { "gdscript", "godot_resource" },
+    },
+  },
+
+  {
+    "mason-org/mason.nvim",
+    optional = true,
+    opts_extend = { "ensure_installed" },
+    opts = { ensure_installed = { "gdscript-formatter", "gdtoolkit" } },
+  },
+
+  -- linter
+  {
+    "mfussenegger/nvim-lint",
+    optional = true,
+    opts = {
+      linters_by_ft = {
+        gdscript = { "gdlint" },
+      },
+    },
+  },
+
+  -- formatter
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        gdscript = { "gdscript-formatter" },
+      },
+    },
+  },
+}
