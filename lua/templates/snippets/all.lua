302316dc4704441c247215b50180eaafe44d751d
Zexi. Li <lzx12138@sjtu.edu.cn>
2026-04-13 13:53:03 +0800

    feat(snippet): added todo-comment style snippet for all filetypes.

--- lua/templates/snippets/all.lua
+++ lua/templates/snippets/all.lua
@@ -1,4 +1,8 @@
+local todo_comment_snippets = require("templates.snippets.generators.todo-comment").generate_todo_comment_snippets()
+
 return {
   s({ trig = "date", desc = "Date" }, { t(os.date("%Y.%m.%d")) }),
   s({ trig = "datetime", desc = "Date & time" }, { t(os.date("%Y.%m.%d %H:%M:%S")) }),
+
+  unpack(todo_comment_snippets),
 }
