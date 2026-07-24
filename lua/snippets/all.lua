093b98c3e2e4befa856132f5575ca81de641c336
Zexi. Li <lzx12138@sjtu.edu.cn>
2026-07-24 14:21:23 +0800

    fix(snippet): fixed the print of date and datetime for the current time instead of the nvim start time.

--- lua/snippets/all.lua
+++ lua/snippets/all.lua
@@ -1,8 +1,22 @@
 local todo_comment_snippets = require("snippets.generators.todo-comment").generate_todo_comment_snippets()
 
+local function date_and_time(
+  _,
+  _, -- parent snippet or parent node
+  user_args -- user_args from opts.user_args
+)
+  -- require("snacks.notify").info(user_args)
+
+  if user_args == "date" then
+    return os.date("%Y.%m.%d")
+  else -- user_args[0] == "datetime"
+    return os.date("%Y.%m.%d %H:%M:%S")
+  end
+end
+
 return {
-  s({ trig = "date", desc = "Date" }, { t(os.date("%Y.%m.%d")) }),
-  s({ trig = "datetime", desc = "Date & time" }, { t(os.date("%Y.%m.%d %H:%M:%S")) }),
+  s({ trig = "date", desc = "Date" }, { f(date_and_time, {}, { user_args = { "date" } }) }),
+  s({ trig = "datetime", desc = "Date & time" }, { f(date_and_time, {}, { user_args = { "datetime" } }) }),
 
   unpack(todo_comment_snippets),
 }
